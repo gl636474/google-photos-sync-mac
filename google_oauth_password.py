346003f5ecbe835a3f5366c6fd0358826e51d678
gareth <gareth@gareths-mac-mini.local>
2020-10-28 09:11:14 +0000

    Fixes #5 - removed secrets from code

--- google_oauth_password.py
+++ google_oauth_password.py
@@ -2,7 +2,7 @@ from oauthlib.oauth2 import LegacyApplicationClient
 from requests_oauthlib import OAuth2Session
 
 client_id = '1021661992594-ir7k24li0c6l9595nrc14aqh1dr8aphp.apps.googleusercontent.com'
-client_secret = 'u-FX-g4kNTKxHErjD73-St4t'
+client_secret = input('client_secret: ')
 auth_uri = 'https://accounts.google.com/o/oauth2/auth'
 token_uri = 'https://oauth2.googleapis.com/token'
 scopes = ['https://www.googleapis.com/auth/photoslibrary.readonly']
